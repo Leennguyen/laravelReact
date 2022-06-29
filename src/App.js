cb3ce4ef81649c64dfd8e336d0702d0bcae07403
Leennguyen <nguyenthilinh26112002@gmail.com>
2022-06-29 11:38:02 +0700

    commit mor 29/06

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "bootstrap/dist/css/bootstrap.css";
+
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+//import EditCar from "./components/edit.component";
+import CarList from "./components/list.component";
+import CreateCar from "./components/create.component";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar bg="primary">
+        <Container>
+          <Link to={"/"} className="navbar-brand text-white">
+            Car
+          </Link>
+        </Container>
+      </Navbar>
+
+      <Container className="mt-5">
+        <Row>
+          <Col md={12}>
+            <Routes>
+              <Route path="/car/create" element={<CreateCar />} />
+              {/*<Route path="/car/edit/:id" element={<EditCar />} /> */}
+              <Route exact path="/cars" element={<CarList />} />
+            </Routes>
+          </Col>
+        </Row>
+      </Container>
+    </Router>
   );
 }
 
